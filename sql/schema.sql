dcb139aa0b94217b9e21623369b978399504a48b
Brendon Oliver <brendon.oliver@gmail.com>
2012-03-01 13:02:02 +1100

    Separate data from DDL into new schema.sql file.
    - Removed "personal" data from old TankTracker.sql into
      separate data.sql (and amended .gitignore to not track this file).

---
+++ sql/schema.sql
@@ -0,0 +1,130 @@
+--\set ON_ERROR_STOP 1
+BEGIN TRANSACTION;
+
+CREATE TABLE tracker_user (
+	user_id		SERIAL NOT NULL,
+	first_name	TEXT NOT NULL,
+	last_name	TEXT,
+	email_address	TEXT NOT NULL,
+	passwd		TEXT NOT NULL,
+	admin		BOOLEAN NOT NULL DEFAULT FALSE,
+
+	PRIMARY KEY ( user_id )
+);
+CREATE UNIQUE INDEX email_address_idx ON tracker_user ( lower(email_address) );
+
+CREATE TABLE session (
+	session_id      BIGINT NOT NULL,
+	user_id		INTEGER NOT NULL,
+	created_on	TIMESTAMP(0) NOT NULL,
+
+	PRIMARY KEY ( session_id ),
+
+	FOREIGN KEY ( user_id ) REFERENCES tracker_user ( user_id ) ON DELETE CASCADE
+);
+
+CREATE TABLE water_type (
+	water_id	SERIAL NOT NULL,
+	water_type	TEXT NOT NULL,
+
+	PRIMARY KEY ( water_id )
+);
+
+INSERT INTO water_type VALUES ( default, 'Salt Water'  );
+INSERT INTO water_type VALUES ( default, 'Fresh Water' );
+
+CREATE TABLE tank (
+	tank_id		SERIAL NOT NULL,
+	water_id	SERIAL NOT NULL,
+	tank_name	TEXT NOT NULL,
+	user_id		INTEGER NOT NULL,
+	notes           TEXT,
+	updated_on	TIMESTAMP(0) DEFAULT now(),
+
+	PRIMARY KEY ( tank_id ),
+	FOREIGN KEY ( user_id  ) REFERENCES tracker_user ( user_id  ),
+	FOREIGN KEY ( water_id ) REFERENCES water_type   ( water_id )
+);
+
+CREATE TABLE tank_user (
+	tank_id	INTEGER NOT NULL,
+	user_id	INTEGER NOT NULL,
+	admin	BOOLEAN DEFAULT FALSE,
+	
+	PRIMARY KEY ( tank_id, user_id ),
+	FOREIGN KEY ( tank_id ) REFERENCES tank ( tank_id  ),
+	FOREIGN KEY ( user_id ) REFERENCES tracker_user ( user_id  )
+);
+
+CREATE TABLE water_tests (
+	test_id		  SERIAL NOT NULL,
+	tank_id		  INTEGER NOT NULL,
+	user_id		  INTEGER NOT NULL,
+	test_date	  DATE NULL DEFAULT current_date,
+	result_salinity	  NUMERIC,
+	result_ph	  NUMERIC,
+	result_ammonia	  NUMERIC,
+	result_nitrite    NUMERIC,
+	result_nitrate	  NUMERIC,
+	result_calcium	  NUMERIC,
+	result_phosphate  NUMERIC,
+	result_magnesium  NUMERIC,
+	result_kh  	  NUMERIC,
+	result_alkalinity NUMERIC,
+	water_change	  NUMERIC,
+	notes		  TEXT,
+
+	PRIMARY KEY ( test_id ),
+	FOREIGN KEY ( tank_id ) REFERENCES tank ( tank_id ),
+	FOREIGN KEY ( user_id ) REFERENCES tracker_user ( user_id )
+);
+/* Not sure whether to allow multiple tests to be run on same day/date: */
+-- CREATE UNIQUE INDEX ON water_tests (tank_id, ( test_timestamp::date ));
+
+CREATE TABLE tank_diary (
+	diary_id	SERIAL NOT NULL,
+	tank_id		INTEGER NOT NULL,
+	user_id		INTEGER NOT NULL,
+	diary_date	TIMESTAMP(0) NOT NULL DEFAULT now(),
+	diary_note	TEXT NOT NULL,
+	updated_on      TIMESTAMP(0) NOT NULL DEFAULT now(),
+	test_id		INT,
+	
+	PRIMARY KEY ( diary_id ),
+	FOREIGN KEY ( tank_id  ) REFERENCES tank ( tank_id ),
+	FOREIGN KEY ( user_id  ) REFERENCES tracker_user ( user_id ),
+	FOREIGN KEY ( test_id  ) REFERENCES water_tests  ( test_id ) ON DELETE CASCADE
+);
+CREATE INDEX tank_diary_date ON tank_diary ( tank_id, diary_date );
+
+CREATE TABLE inventory_type (
+	inventory_type_id SERIAL NOT NULL,
+	description	  TEXT NOT NULL,
+
+	PRIMARY KEY ( inventory_type_id )
+);
+
+INSERT INTO inventory_type VALUES (default, 'Consumable');
+INSERT INTO inventory_type VALUES (default, 'Equipment');
+INSERT INTO inventory_type VALUES (default, 'Fish');
+INSERT INTO inventory_type VALUES (default, 'Invertebrate');
+INSERT INTO inventory_type VALUES (default, 'Coral');
+
+CREATE TABLE inventory (
+	item_id		  SERIAL NOT NULL,
+	inventory_type_id INTEGER NOT NULL,
+	tank_id		  INTEGER NOT NULL,
+	user_id		  INTEGER NOT NULL,
+	description	  TEXT NOT NULL,
+	purchase_date	  DATE NOT NULL,
+	purchase_price	  MONEY NOT NULL,
+
+	PRIMARY KEY ( item_id ),
+	FOREIGN KEY ( tank_id  ) REFERENCES tank ( tank_id ),
+	FOREIGN KEY ( user_id )  REFERENCES tracker_user ( user_id ),
+	FOREIGN KEY ( inventory_type_id  ) REFERENCES inventory_type ( inventory_type_id ) ON DELETE CASCADE
+);
+
+
+
+COMMIT;
