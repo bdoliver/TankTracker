d6233a608fc14b6c41d57ef754503d98aeb10c5c
Brendon Oliver <brendon.oliver@gmail.com>
2012-03-01 14:06:51 +1100

    Fix broken "get '/pages'" behaviour.
    This route handler had not been properly reworked after the conversion from
    Rose::DB to DBIx::Class.  Now fixed.

--- lib/TankTracker/Route/Diary.pm
+++ lib/TankTracker/Route/Diary.pm
@@ -18,7 +18,7 @@ prefix '/diary';
 get '/' => sub {
 	my $tank_id = params->{'tank_id'};
 
-	my $tank    = schema->resultset('TankTracker')->find($tank_id);
+	my $tank    = schema->resultset('Tank')->find($tank_id);
 
 	template 'diary.tt', 
 		 { tank_id => $tank_id },
@@ -50,47 +50,43 @@ get '/pages' => sub {
 	my $pages = schema->resultset('TankDiary')->search($query,
 							   { 'order_by' => 'diary_id DESC' });
 
-	my @pages = ();
+	my ( @pages, $item, $i, $c );
 
-	my $total_pg = @$pages;
+	my $total_pg = $pages->count();
 
-	if ( $pages and @$pages ) {
-		my ( $item, $i, $c );
+	my $start = ( $rec_pp * $curr_pg ) - $rec_pp + 1;
 
-		my $start = ( $rec_pp * $curr_pg ) - $rec_pp + 1;
+	while ( my $p = $pages->next() ) {
 
-		for my $p ( @$pages ) {
+		# 'item' is just a sequential number so that the
+		# diary entries appear ordered in the UI:
+		++$item;
 
-			# 'item' is just a sequential number so that the
-			# diary entries appear ordered in the UI:
-			++$item;
+		my $id   = $p->diary_id();
 
-			my $id   = $p->diary_id();
+		my $note =  $p->diary_note();
+			$note =~ s|<|&lt;|msg;
+			$note =~ s|>|&gt;|msg;
+			$note =~ s|\n|<br />|msg;
 
-			my $note =  $p->diary_note();
-			   $note =~ s|<|&lt;|msg;
-			   $note =~ s|>|&gt;|msg;
-			   $note =~ s|\n|<br />|msg;
+		my $upd  =  $p->updated_on();
+			$upd  =~ s|T|<br />|;
 
-			my $upd  =  $p->updated_on();
-			   $upd  =~ s|T|<br />|;
+		my $page =  { item       => $item,
+				diary_id   => $id,
+				diary_date => $p->diary_date()->ymd(),
+				diary_note => $note,
+				updated_on => $upd,
+				test_id    => $p->test_id() };
 
-			my $page =  { item       => $item,
-				      diary_id   => $id,
-				      diary_date => $p->diary_date()->ymd(),
-				      diary_note => $note,
-				      updated_on => $upd,
-				      test_id    => $p->test_id() };
+		if ( ! $rec_pp ) {
+			push @pages, $page;
+			next;
+		}
 
-			if ( ! $rec_pp ) {
-				push @pages, $page;
-				next;
-			}
-
-			if ( ++$i >= $start and ++$c <= $rec_pp ) {
-				push @pages, $page;
-				last if $c == $rec_pp;
-			}
+		if ( ++$i >= $start and ++$c <= $rec_pp ) {
+			push @pages, $page;
+			last if $c == $rec_pp;
 		}
 	}
 
