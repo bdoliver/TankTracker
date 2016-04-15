161c0ccce3cf47105d2b64f810ffca4222fcaccc
Brendon Oliver <brendon.oliver@gmail.com>
2016-04-15 14:50:06 +1000

    Playing with require.js ... maybe....

---
+++ root/static/js/require-setup.js
@@ -0,0 +1,21 @@
+var require = {
+    shim : {
+        "bootstrap": { "deps" :['jquery'] }
+    },
+    paths: {
+        "jquery"               : 'jquery-2.1.4.min.js',
+        "bootstrap"            : 'bootstrap.min.js'
+        "bootstrap-datepicker" : 'bootstrap-datepicker.min.js',
+        "bootstrap-dialog"     : 'bootstrap-dialog.min.js',
+        "colpick"              : 'colpick.js',
+        "fileinput"            : 'fileinput.min.js',
+        "flot"                 : 'jquery.flot.min.js',
+        "flot-navigate"        : 'jquery.flot.navigate.min.js',
+        "flot-resize"          : 'jquery.flot.resize.min.js',
+        "flot-time"            : 'jquery.flot.time.min.js',
+        "moment"               : 'moment.min.js',
+        "moment-strftime"      : 'moment-strftime-0.1.2.min.js'
+    }
+};
+
+
---
+++ root/static/js/tanktracker.js
@@ -0,0 +1,9 @@
+require(['jquery', 'bootstrap'], function($){
+
+    // DOM ready
+    // $(function(){
+    //
+    //     // Twitter Bootstrap 3 carousel plugin
+    //     $("#element").carousel();
+    // });
+});
