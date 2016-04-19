c32de77a71bd9be36d2936ae7e0d0a4d738d76b1
Brendon Oliver <brendon.oliver@gmail.com>
2016-04-20 09:35:26 +1000

    Shifted javascript libs off to separate /lib dir.

--- root/static/js/require-setup.js
+++ root/static/js/require-setup.js
@@ -1,26 +1,36 @@
 var require = {
     baseUrl: "/static/js",
     shim   : {
-        "bootstrap"   : ['jquery'],
-        "colpick"     : ['jquery'],
-        "jquery.flot" : {
-            'deps'    : [ 'jquery' ],
+ 'jquery': { exports: '$' },
+        'bootstrap'   : ['jquery'],
+        'colpick'     : ['jquery'],
+        'jquery.flot' : {
+            'deps'    : ['jquery'],
             'exports' : '$.plot'
+        },
+        'jquery.flot.time' : {
+            'deps'    : ['jquery.flot']
+        },
+        'jquery.flot.navigate' : {
+            'deps'    : ['jquery.flot']
+        },
+        'jquery.flot.resize' : {
+            'deps'    : ['jquery.flot']
         }
     },
     paths: {
-        "jquery"               : 'jquery-2.1.4.min',
-        "bootstrap"            : 'bootstrap.min',
-        "bootstrap-datepicker" : 'bootstrap-datepicker.min',
-        "bootstrap-dialog"     : 'bootstrap-dialog.min',
-        "colpick"              : 'colpick',
-        "fileinput"            : 'fileinput.min',
-        "jquery.flot"          : 'jquery.flot.min',
-        "jquery.flot.navigate" : 'jquery.flot.navigate.min',
-        "jquery.flot.resize"   : 'jquery.flot.resize.min',
-        "jquery.flot.time"     : 'jquery.flot.time.min',
-        "moment"               : 'moment.min',
-        "moment-strftime"      : 'moment-strftime-0.1.2.min'
+        "jquery"               : 'lib/jquery-2.1.4.min',
+        "bootstrap"            : 'lib/bootstrap.min',
+        "bootstrap-datepicker" : 'lib/bootstrap-datepicker.min',
+        "bootstrap-dialog"     : 'lib/bootstrap-dialog.min',
+        "colpick"              : 'lib/colpick',
+        "fileinput"            : 'lib/fileinput.min',
+        "jquery.flot"          : 'lib/jquery.flot.min',
+        "jquery.flot.navigate" : 'lib/jquery.flot.navigate.min',
+        "jquery.flot.resize"   : 'lib/jquery.flot.resize.min',
+        "jquery.flot.time"     : 'lib/jquery.flot.time.min',
+        "moment"               : 'lib/moment.min',
+        "moment-strftime"      : 'lib/moment-strftime-0.1.2.min'
     }
 };
 
